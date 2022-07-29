891146f9262ffbe3b31252369715beae0b0f32a1
DaraGinger <darinafrouk>
2022-07-29 17:39:29 +0300

    Add enter

---
+++ Exchange/Exchange/Exchange/Buy.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Exchange
+{
+    public partial class Buy : Form
+    {
+        public Buy()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateUsdButton_Click(object sender, EventArgs e)
+        {
+            Exchange rates = (Exchange)Application.OpenForms[0];
+
+            double usd = Convert.ToDouble(UsdText.Text);
+            double uah = Convert.ToDouble(UahUsdText.Text);
+            double usdMain = Convert.ToDouble(rates.UsdBuyMainText.Text);
+
+            if(UsdText.Text.Length == 0)
+            {
+                double quotioent = uah/usdMain;
+                UsdText.Text = Convert.ToString(quotioent);
+            }
+
+            else
+            {
+                double pro = usd*usdMain;
+                UahUsdText.Text = Convert.ToString(pro);
+            }
+
+
+        }
+
+        private void CalculateEurButton_Click(object sender, EventArgs e)
+        {
+            Exchange rates = (Exchange)Application.OpenForms[0];
+
+            double usdMain = Convert.ToDouble(rates.EurBuyMainText.Text);
+
+            if (UsdText.Text.Length == 0)
+            {
+                double uah = Convert.ToDouble(UahUsdText.Text);
+                double quotioent = uah/usdMain;
+                UsdText.Text = Convert.ToString(quotioent);
+            }
+
+            else
+            {
+                double eur = Convert.ToDouble(EurText.Text);
+                double pro = eur*usdMain;
+                UahUsdText.Text = Convert.ToString(pro);
+            }
+        }
+
+        private void CalculatePlnButton_Click(object sender, EventArgs e)
+        {
+            Exchange rates = (Exchange)Application.OpenForms[0];
+
+            double pln = Convert.ToDouble(PlnText.Text);
+            double uah = Convert.ToDouble(UahUsdText.Text);
+            double plnMain = Convert.ToDouble(rates.PlnBuyMainText.Text);
+
+            if (UsdText.Text.Length == 0)
+            {
+                double quotioent = uah/plnMain;
+                UsdText.Text = Convert.ToString(quotioent);
+            }
+
+            else
+            {
+                double pro = pln*plnMain;
+                UahUsdText.Text = Convert.ToString(pro);
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            Exchange exchangePage = (Exchange)Application.OpenForms[0];
+            exchangePage.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Exchange/Exchange/Exchange/Exchange.cs
@@ -0,0 +1,96 @@
+﻿namespace Exchange
+{
+    using System;
+    using System.Windows.Forms;
+    using System.IO;
+
+    public partial class Exchange : Form
+    {
+        Update updateForm;
+        public Exchange()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            updateForm = new Update();
+            updateForm.Show();
+            
+            updateForm.Owner = this;
+            this.Hide(); 
+            
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+
+            if (updateForm.UsdBuyText.Text.Length == 0)
+            {
+                UsdBuyMainText.Text = UsdBuyMainText.Text;
+            }
+            else
+            {
+                UsdBuyMainText.Text = updateForm.UsdBuyText.Text;
+            }
+
+
+            if (updateForm.UsdSellText.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                UsdSellMainText.Text = updateForm.UsdSellText.Text;
+            }
+
+
+            if (updateForm.EurBuyText.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                EurBuyMainText.Text = updateForm.EurBuyText.Text;
+            }
+
+
+            if (updateForm.EurSellText.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                EurSellMainText.Text = updateForm.EurSellText.Text;
+            }
+
+
+            if (updateForm.PlnBuyText.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                PlnBuyMainText.Text = updateForm.PlnBuyText.Text;
+            }
+
+            if (updateForm.PlnSellText.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                PlnSellMainText.Text = updateForm.PlnSellText.Text;
+            }
+        }
+
+        private void BuyingButton_Click(object sender, EventArgs e)
+        {
+            Buy buyPage = new Buy();
+            buyPage.Show();
+
+            this.Hide();
+
+        }
+    }
+}
